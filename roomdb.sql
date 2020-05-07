b4e5eede1b048fabeb4bf81f7f62fedbc24ebfb4
robinettec4 <60123910+robinettec4@users.noreply.github.com>
2020-05-07 19:38:34 -0400

    Add files via upload

--- roomdb.sql
+++ roomdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2020 at 09:00 PM
+-- Generation Time: May 08, 2020 at 01:38 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -25,25 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comments`
---
-
-CREATE TABLE `comments` (
-  `name` varchar(40) NOT NULL,
-  `comment` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `comments`
---
-
-INSERT INTO `comments` (`name`, `comment`) VALUES
-('Connor Robinette', 'test comment'),
-('Robinette Connor', 'comment test');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `creditcard`
 --
 
@@ -53,8 +34,17 @@ CREATE TABLE `creditcard` (
   `cardNumber` bigint(16) UNSIGNED NOT NULL,
   `cardExpiration` date NOT NULL,
   `cardSecurityNumber` int(3) NOT NULL,
-  `Accepted` tinyint(1) NOT NULL
+  `cardHolder` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `creditcard`
+--
+
+INSERT INTO `creditcard` (`ID`, `customerID`, `cardNumber`, `cardExpiration`, `cardSecurityNumber`, `cardHolder`) VALUES
+(2, 15, 1111222233334444, '2020-05-07', 123, 'Connor Robinette'),
+(3, 15, 2222333344445555, '2020-05-20', 123, 'Connor Robinette'),
+(4, 15, 1234123412341234, '2020-01-01', 321, 'Connor Robinette');
 
 -- --------------------------------------------------------
 
@@ -76,9 +66,8 @@ CREATE TABLE `customer` (
 
 INSERT INTO `customer` (`ID`, `name`, `email`, `password`, `userType`) VALUES
 (13, 'admin', 'admin@gmail.com', '$2y$10$6NUv.w.xgjwVXD0pMiv/2eylhYIlqlfuylEndklloPU7de2n1A.1K', 1),
-(15, 'Connor Robinette', 'connor@gmail.com', '$2y$10$evi9m7kzwiZIKyQpfSdpkeyKsL18loPZOtbmHLQA2KDTGa9r5BZ0m', 0),
-(17, 'Test Testington', 'test@gmail.com', '$2y$10$lTd73HXUDp5MpXYYhEW9KecuG74BMnupk5ug8P4LPTDEShjbUQmVK', 1),
-(18, 'idiot dumb', 'gmail@gmail.com', '$2y$10$KGhK7DJDLkaaZ9yDuIiSbO7oxXEbFdu6dQe3.ncaraSFCKbWxdqRK', 0);
+(15, 'Connor Robinette', 'connor@gmail.com', '$2y$10$evi9m7kzwiZIKyQpfSdpkeyKsL18loPZOtbmHLQA2KDTGa9r5BZ0m', 1),
+(21, 'idiot dumb', 'idiot@gmail.com', '$2y$10$TCUJ/fKB3TKyiBgIespVhOQpMH48nUEfoKQMUNTg7S7r.EIF.TkxK', 0);
 
 -- --------------------------------------------------------
 
@@ -92,18 +81,21 @@ CREATE TABLE `hotel` (
   `email` varchar(40) CHARACTER SET utf8 DEFAULT NULL,
   `availableRooms` int(11) NOT NULL,
   `name` varchar(40) CHARACTER SET utf8 DEFAULT NULL,
-  `picture` varchar(40) CHARACTER SET utf8 DEFAULT NULL
+  `picture` varchar(40) CHARACTER SET utf8 DEFAULT NULL,
+  `price` double UNSIGNED NOT NULL,
+  `description` text NOT NULL,
+  `size` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `hotel`
 --
 
-INSERT INTO `hotel` (`ID`, `address`, `email`, `availableRooms`, `name`, `picture`) VALUES
-(1, '1 Average Avenue', 'joe.average@gmail.com', 3, 'Joe Average Hotel', 'picture/average.jpg'),
-(2, '2 Exceptional Expressway', 'exceptionalhotels@gmail.com', 5, 'Exceptionally Overpriced Hotel', 'picture/overpriced.jpg'),
-(3, '3 Reasonable Road', 'reasonable.ralph@gmail.com', 7, 'Reasonable Ralph\'s Hotel', 'picture/reasonable.jpg'),
-(4, '4 Ripoff Route', 'rick.ripoff@gmail.com', 20, 'Ripoff Rick\'s Hotel', 'picture/ripoff.jpg');
+INSERT INTO `hotel` (`ID`, `address`, `email`, `availableRooms`, `name`, `picture`, `price`, `description`, `size`) VALUES
+(1, '1 Average Avenue', 'joe.average@gmail.com', 3, 'Joe Average Hotel', 'average.jpg', 50, 'An average hotel at an average price!', 4),
+(2, '2 Exceptional Expressway', 'exceptionalhotels@gmail.com', 2, 'Exceptionally Overpriced Hotel', 'overpriced.jpg', 150, 'A very expensive hotel', 2),
+(3, '3 Reasonable Road', 'reasonable.ralph@gmail.com', 2, 'Reasonable Ralph\'s Hotel', 'reasonable.jpg', 100, 'Reasonably priced hotel', 2),
+(4, '4 Ripoff Route', 'rick.ripoff@gmail.com', 2, 'Ripoff Rick\'s Hotel', 'ripoff.jpg', 500, 'Don\'t stay here!', 1);
 
 -- --------------------------------------------------------
 
@@ -115,11 +107,19 @@ CREATE TABLE `reservation` (
   `ID` int(11) UNSIGNED NOT NULL,
   `roomID` int(11) UNSIGNED NOT NULL,
   `customerID` int(11) UNSIGNED NOT NULL,
-  `numberOfGuests` int(11) NOT NULL,
   `dayStart` date NOT NULL,
   `dayEnd` date NOT NULL,
   `creditCardID` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`ID`, `roomID`, `customerID`, `dayStart`, `dayEnd`, `creditCardID`) VALUES
+(21, 5, 15, '2020-05-05', '2020-05-05', 2),
+(22, 9, 15, '2020-05-05', '2020-05-05', 2),
+(23, 3, 15, '2020-05-05', '2020-05-07', 2);
 
 -- --------------------------------------------------------
 
@@ -130,23 +130,23 @@ CREATE TABLE `reservation` (
 CREATE TABLE `room` (
   `ID` int(10) UNSIGNED NOT NULL,
   `hotelID` int(10) UNSIGNED NOT NULL,
-  `price` double UNSIGNED NOT NULL,
-  `size` tinyint(3) UNSIGNED NOT NULL,
-  `picture` varchar(40) CHARACTER SET utf8 DEFAULT NULL,
-  `description` text CHARACTER SET utf8 DEFAULT NULL
+  `roomNumber` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `room`
 --
 
-INSERT INTO `room` (`ID`, `hotelID`, `price`, `size`, `picture`, `description`) VALUES
-(1, 1, 100, 2, 'picture/average.jpg', 'This is the description for room 1'),
-(2, 2, 900, 1, 'picture/overpriced.jpg', 'This is the description for room 2'),
-(3, 3, 150, 4, 'picture/reasonable.jpg', 'This is the description for room 3'),
-(4, 4, 200, 2, 'picture/ripoff.jpg', 'This is the description for room 4'),
-(5, 1, 90, 2, 'average.jpg', 'This is the description for this room'),
-(9, 1, 90, 2, 'average.jpg', '1');
+INSERT INTO `room` (`ID`, `hotelID`, `roomNumber`) VALUES
+(1, 1, 100),
+(2, 2, 100),
+(3, 3, 100),
+(4, 4, 100),
+(5, 1, 200),
+(9, 1, 300),
+(10, 2, 200),
+(11, 1, 201),
+(15, 4, 200);
 
 --
 -- Indexes for dumped tables
@@ -198,31 +198,31 @@ ALTER TABLE `room`
 -- AUTO_INCREMENT for table `creditcard`
 --
 ALTER TABLE `creditcard`
-  MODIFY `ID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `ID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `ID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `hotel`
 --
 ALTER TABLE `hotel`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `ID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
